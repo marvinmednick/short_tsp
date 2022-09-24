9a5ae384dc8c09971fe12547c41ca4aaf2035c78
Marvin Mednick <mmednick@gmail.com>
2022-09-24 15:22:49 -0700

    moved TSP to its own mod

--- src/tsp.rs
+++ src/tsp.rs
@@ -1,25 +1,23 @@
-mod tsp;
-mod unidirgraph;
-use crate::tsp::TSP;
-use log::{  info ,/* error ,*/ debug, /* warn ,*/ trace };
-/*
 use std::collections::{BTreeMap, BTreeSet};
 use itertools::Itertools;
 
 use crate::unidirgraph::UnidirectionalGraph;
 
+use log::{  info ,/* error ,*/ debug, /* warn ,*/ trace };
 
 #[derive(Debug,Clone)]
-struct PathInfo {
+pub struct PathInfo {
     distance: i64,
     prev: usize,
 }
 
-struct TSP {
+pub struct TSP {
     pub vertex: BTreeSet<usize>,
     pub  vertex_sets :  Vec<BTreeSet<usize>>,
     path_calcs : BTreeMap<(BTreeSet<usize>,usize),PathInfo>,
     pub graph: UnidirectionalGraph,
+    tsp_path:  Vec<usize>,
+    tsp_distance: i64,
 }
 
 impl TSP {
@@ -30,6 +28,8 @@ impl TSP {
             vertex_sets: Vec::<BTreeSet<usize>>::new(),
             path_calcs : BTreeMap::<(BTreeSet::<usize>,usize),PathInfo>::new(),
             graph: UnidirectionalGraph::new(),
+            tsp_path:  Vec::<usize>::new(),
+            tsp_distance: i64::MAX,
         }
 
     }
@@ -64,7 +64,7 @@ impl TSP {
 
     }
 
-    pub fn find_path(&self, vertex_set: &BTreeSet<usize>, last_vertex: usize) {
+    pub fn find_path(&self, vertex_set: &BTreeSet<usize>, last_vertex: usize) -> Vec<usize> {
         let mut path = Vec::<usize>::new();
         let mut reduced_set = vertex_set.clone();
 
@@ -80,6 +80,7 @@ impl TSP {
             cur_vertex = previous;
         }
         trace!("TSP Path is {:?}",path);
+        path
 
     }
 
@@ -145,8 +146,9 @@ impl TSP {
                 final_vertex = *last_vertex;
             }
         }
-        println!("TSP Distance {} last vertex is {}", min_distance,final_vertex);use std::process::exit;
-        self.find_path(&self.vertex,final_vertex);
+        println!("TSP Distance {} last vertex is {}", min_distance,final_vertex);
+        self.tsp_path = self.find_path(&self.vertex,final_vertex);
+        self.tsp_distance = min_distance;
     }
 
     pub fn define_vertex(&mut self, vertex: usize) {
@@ -157,121 +159,10 @@ impl TSP {
         self.graph.define_edge(v1,v2,distance);
     }
 
-}
-*/
 
-fn main() {
-    
-    env_logger::init();
-
-//    let mut g = UnidirectionalGraph::new();
-    let mut tsp = TSP::new();
-/*
-    let mut i = 1;
-    g.define_edge(1,2,i);   i+=1;
-    g.define_edge(3,2,i);   i+=1;
-    g.define_edge(3,4,i);   i+=1;
-    g.define_edge(4,5,i);   i+=1;
-    g.define_edge(1,3,i);   i+=1;
-    g.define_edge(1,4,i);   i+=1;
-    g.define_edge(1,5,i);   i+=1;
-    g.define_edge(2,4,i);   i+=1;
-    g.define_edge(2,5,i);   i+=1;
-    g.define_edge(3,5,i);   
-*/
-    let mut i = 1;
-    tsp.define_edge(1,2,i);   i+=1;
-    tsp.define_edge(3,2,i);   i+=1;
-    tsp.define_edge(3,4,i);   i+=1;
-    tsp.define_edge(4,5,i);   i+=1;   
-    tsp.define_edge(1,5,i);   i+=1;
-    tsp.define_edge(1,4,i);   i+=1;
-    tsp.define_edge(1,3,i);   i+=1;
-    tsp.define_edge(2,4,i);   i+=1;
-    tsp.define_edge(2,5,i);   i+=1;
-    tsp.define_edge(3,5,i);   // i+=1;
-
-    info!("Edges");
-    for edge in &tsp.graph.edges {
-        info!("{:?}",edge);
+    pub fn solution(&self) -> (i64, &Vec<usize>) {
+        (self.tsp_distance,&self.tsp_path)
     }
-    info!("---------------");
-/*
- //   let range : Vec<usize> = (2..=5).into_iter().collect();
- //   println!("Range {:?}",range);
-//    let mut vertex : BTreeSet<usize> = BTreeSet::<usize>::from_iter(range.iter().cloned());
-    //let mut vertex : BTreeSet<usize> = BTreeSet::<usize>::from_iter(g.vertex_iter().cloned());
-    println!("Vertex {:?}",vertex);
-//    println!("Vertex1 {:?}",vertex);
-    let mut vertex_set = Vec::<BTreeSet<usize>>::new();
-
-    //remove the starting vertex
-    let starting_vertex : usize = 1;
-    vertex.remove(&starting_vertex);
-    
-    for size in 0..vertex.len()+1 {
-        let vset = vertex.iter().combinations(size);
-        for combo in vset {
-            // for each set of combination of len 'size'
-            // create 
-            let set = BTreeSet::<usize>::from_iter(
-                combo.into_iter().cloned().collect::<Vec<usize>>());
-            println!("set {:?}", set );
-            vertex_set.push(set.clone());
-    //        tsp.add_set(set);
-        }
-    }
-    println!("Vertex Set {:?}", vertex_set);
-    println!("G Vertex sets {:?}", tsp.vertex_sets);
-*/
-    trace!("Calling Init");
-    tsp.initialize(1);
-
-    trace!("Calling Calcuate");
-    tsp.calculate_tsp_path();
-    let (distance, path) = tsp.solution();
-    println!("TSP Distance {}   Path is {:?}", distance, path);
-
-/*
-    exit(0);
-    println!("Old ..  ");
-
-    let mut tsp_calc = BTreeMap::<BTreeSet::<usize>,i64>::new();
-    for set in vertex_set {
-        println!("Set {:?} ", set);
-        let mut reduced_set = set.clone();
-        for v in &set {
-            reduced_set.remove(v);
-            println!(" {:?} -> v:{} Min of:", reduced_set, v);
-            if reduced_set.is_empty() {
-                let edge = UnidirectionalGraph::edge_name(1,*v);
-                let edge_distance = g.get_distance(1,*v);
-                println!(" Edge (1,{}) i.e {:?} {}", v,edge, edge_distance);
-                tsp_calc.insert(set.clone(),edge_distance);
-            }
-            else {
-                let mut min_distance = i64::MAX;
-                for source in &reduced_set {
-                    let edge = UnidirectionalGraph::edge_name(*source,*v);
-                    let edge_distance = g.get_distance(*source,*v);
-                    let set_weight = tsp_calc.get(&reduced_set).unwrap();
-                    let new_dist = set_weight + edge_distance;
-                    print!( "   Set {:?} to {} via {}: set dist: {} + edge ({},{}) dist {} = total {}  cur_min {})",reduced_set, v, source, set_weight, source, v, edge_distance, new_dist, min_distance);
-                    if new_dist < min_distance {
-                        min_distance = new_dist;
-                        tsp_calc.insert(set.clone(),new_dist);
-                        println!(" - Updating {:?} to {}",set,new_dist);
-                    }
-                    else {
-                        println!(" - Skipping");
-                    }
-
-                }
-            }
-            reduced_set.insert(*v);
-        }
-        println!();
-    }
-*/
 
 }
+
